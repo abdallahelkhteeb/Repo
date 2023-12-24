844fc542f698d863a27aaa9fc64a5f198b13a9da
Abduallah <abdalllaabdalllla@gmail.com>
2023-12-24 23:54:01 +0200

    Program to find the sum of two numbers

--- file.cpp
+++ file.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 
 int main() {
-    cout << "Hello, world\n";
+    cout << "Enter the first number : ";
+    int num1, num2;
+    cin >> num1;
+    cout << "\nEnter the second number : ";
+    cin << num2;
+
+    cout << "The summation of the first and the second numebr is: " << num1 + num2 << endl;
     return 0;
 }
